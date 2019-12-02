50169e9f2a30714d2567bf1e5c7ba0916e17c8ac
Carlos Trinidad <carlos.trinidad1@ibm.com>
2019-12-02 15:59:40 -0300

    solucionando los bugs pendientes

--- app.py
+++ app.py
@@ -1,4 +1,5 @@
 # import the necessary packages
+import tensorflow as tf
 from keras.models import load_model
 from PIL import Image
 import numpy as np
@@ -13,6 +14,9 @@ def load_model_from_file():
     # load the  Keras model
     global model
     model = load_model('models/famosos_cnn.h5')
+    # save the graph after loading the model
+    global graph
+    graph = tf.get_default_graph()
 
 def prepare_image(image, target):
     # if the image mode is not RGB, convert it
@@ -31,7 +35,6 @@ def predict():
     # initialize the data dictionary that will be returned from the
     # view
     data = {"success": False}
-    load_model_from_file()
 
     # ensure an image was properly uploaded to our endpoint
     if flask.request.method == "POST":
@@ -50,19 +53,20 @@ def predict():
 
             # classify the input image and then initialize the list
             # of predictions to return to the client
-            preds = model.predict(images_predict)
-            print(preds)
-            #results = imagenet_utils.decode_predictions(preds)
-            data["predictions"] = []
+            with graph.as_default():
+                preds = model.predict(images_predict)
+                print(preds)
+                #results = imagenet_utils.decode_predictions(preds)
+                data["predictions"] = []
 
-            # loop over the results and add them to the list of
-            # returned predictions
-            for i in range(len(preds[0])):
-                r = {"type": class_names[i], "probability": float(preds[0][i])}
-                data["predictions"].append(r)
+                # loop over the results and add them to the list of
+                # returned predictions
+                for i in range(len(preds[0])):
+                    r = {"type": class_names[i], "probability": float(preds[0][i])}
+                    data["predictions"].append(r)
 
-            # indicate that the request was a success
-            data["success"] = True
+                # indicate that the request was a success
+                data["success"] = True
 
     # return the data dictionary as a JSON response
     return flask.jsonify(data)
@@ -70,7 +74,7 @@ def predict():
 # if this is the main thread of execution first load the model and
 # then start the server
 if __name__ == "__main__":
-    print(("* Loading Keras model and Flask starting server..."
+    print(("* Loading Keras model and Flask starting server...",
         "please wait until server has fully started"))
-    load_model()
-    app.run()
+    load_model_from_file()
+    app.run(debug=True)
